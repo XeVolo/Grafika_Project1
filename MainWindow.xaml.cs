12285edab9471765ffff17c700576cb849ee04e4
xevolo <xevolo12@gmail.com>
2024-10-05 20:04:11 +0200

    sad

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -22,10 +22,12 @@ namespace Project1
         private Point _startPoint;
         private DrawingMode _currentDravingMode;
         private Line _currentLine;
+        private Line _selectedLine;
         public MainWindow()
         {
             InitializeComponent();
             _currentDravingMode = DrawingMode.Cursor;
+
         }
         private void Canvas_MouseDown(object sender, MouseButtonEventArgs e)
         {
@@ -47,7 +49,11 @@ namespace Project1
                         X2 = _startPoint.X, // Tymczasowo ustawiamy te same współrzędne
                         Y2 = _startPoint.Y
                     };
+                    _currentLine.MouseLeftButtonDown += ChangeProperties;
                     DrawCanvas.Children.Add(_currentLine);
+                    X1CoordinateTextBox.Text = _currentLine.X1.ToString("F0");
+                    Y1CoordinateTextBox.Text = _currentLine.Y1.ToString("F0");
+                    LineThicknessTextBox.Text = _currentLine.StrokeThickness.ToString("F0");
                 }
             }
         }
@@ -93,6 +99,8 @@ namespace Project1
                             // Aktualizujemy współrzędne końcowego punktu linii
                             _currentLine.X2 = currentPoint.X;
                             _currentLine.Y2 = currentPoint.Y;
+                            X2CoordinateTextBox.Text = _currentLine.X2.ToString("F0");
+                            Y2CoordinateTextBox.Text = _currentLine.Y2.ToString("F0");
                         }
                         break;
                     }
@@ -111,10 +119,25 @@ namespace Project1
 
         private void Canvas_MouseUp(object sender, MouseButtonEventArgs e)
         {
-            _isDrawing = false; // Zatrzymanie rysowania
-            _currentLine = null;
+            _isDrawing = false; 
+            if (_currentLine != null)
+            {
+                X2CoordinateTextBox.Text = _currentLine.X2.ToString("F0");
+                Y2CoordinateTextBox.Text = _currentLine.Y2.ToString("F0");
+            }
+            
+            
         }
-
+        private void Reset_Button(object sender, RoutedEventArgs e)
+        {
+            _currentLine = null;
+            _selectedLine = null;
+            X1CoordinateTextBox.Text = "";
+            Y1CoordinateTextBox.Text = "";
+            X2CoordinateTextBox.Text = "";
+            Y2CoordinateTextBox.Text = "";
+            LineThicknessTextBox.Text = "";
+        }
         private void Save_Button(object sender, RoutedEventArgs e)
         {
             
@@ -122,6 +145,65 @@ namespace Project1
         private void Load_Button(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void ChangeProperties(object sender, MouseButtonEventArgs e)
+        {
+            Line clickedLine = sender as Line;
+            if (clickedLine != null)
+            {               
+                _selectedLine = clickedLine;               
+                X1CoordinateTextBox.Text = clickedLine.X1.ToString("F0");
+                Y1CoordinateTextBox.Text = clickedLine.Y1.ToString("F0");
+                X2CoordinateTextBox.Text = clickedLine.X2.ToString("F0");
+                Y2CoordinateTextBox.Text = clickedLine.Y2.ToString("F0");
+                LineThicknessTextBox.Text = clickedLine.StrokeThickness.ToString("F0");
+            }
+        }
+        private void SetProperties_Click(object sender, RoutedEventArgs e)
+        {
+            if (_currentLine == null && _selectedLine == null)
+            {
+                if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
+                    double.TryParse(Y1CoordinateTextBox.Text, out double y1) &&
+                    double.TryParse(X2CoordinateTextBox.Text, out double x2) &&
+                    double.TryParse(Y2CoordinateTextBox.Text, out double y2) &&
+                    double.TryParse(LineThicknessTextBox.Text, out double thickness))
+                {
+                    _currentLine = new Line
+                    {
+                        Stroke = Brushes.Black,
+                        StrokeThickness = thickness,
+                        X1 = x1,
+                        Y1 = y1,
+                        X2 = x2, 
+                        Y2 = y2
+                    };
+                    _currentLine.MouseLeftButtonDown += ChangeProperties;
+                    DrawCanvas.Children.Add(_currentLine);
+                }
+            }
+            else if (_selectedLine != null)
+            {
+                //DrawCanvas.Children.Remove(_selectedLine);
+
+                if (double.TryParse(LineThicknessTextBox.Text, out double thickness))
+                {
+                    _selectedLine.StrokeThickness = thickness;
+                }
+
+                
+                if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
+                    double.TryParse(Y1CoordinateTextBox.Text, out double y1) &&
+                    double.TryParse(X2CoordinateTextBox.Text, out double x2) &&
+                    double.TryParse(Y2CoordinateTextBox.Text, out double y2))
+                {
+                    _selectedLine.X1 = x1; 
+                    _selectedLine.Y1 = y1; 
+                    _selectedLine.X2 = x2; 
+                    _selectedLine.Y2 = y2;
+                }               
+            }
         }
 
         private void Pencil_Selected(object sender, RoutedEventArgs e)
@@ -144,6 +226,6 @@ namespace Project1
         {
             _currentDravingMode = DrawingMode.Circle;
         }
-
+               
     }
 }
