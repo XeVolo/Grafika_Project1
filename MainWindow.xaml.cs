00f5f395e150ee06617de8c2558f58e972d2ffcc
xevolo <xevolo12@gmail.com>
2024-10-06 15:56:53 +0200

    rectangle

--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -18,10 +18,10 @@ namespace Project1
     public partial class MainWindow : Window
     {
         private bool _isDrawing;
-        private Point _previousPoint;
         private Point _startPoint;
         private DrawingMode _currentDravingMode;
         private Line _currentLine;
+        private Rectangle _currentRectangle;
         private Point _offset;
         public MainWindow()
         {
@@ -34,25 +34,51 @@ namespace Project1
             {
                 _startPoint = e.GetPosition(DrawCanvas);
                 _isDrawing = true;
-                _previousPoint = e.GetPosition(DrawCanvas); // Pobierz pozycję kursora
 
-                if (_currentDravingMode.Equals(DrawingMode.Line))
+                switch (_currentDravingMode)
                 {
-                    // Tworzymy nową linię i dodajemy ją do Canvas
-                    _currentLine = new Line
-                    {
-                        Stroke = Brushes.Black,
-                        StrokeThickness = 2,
-                        X1 = _startPoint.X,
-                        Y1 = _startPoint.Y,
-                        X2 = _startPoint.X, // Tymczasowo ustawiamy te same współrzędne
-                        Y2 = _startPoint.Y
-                    };
-                    _currentLine.MouseLeftButtonDown += ChangeProperties;
-                    DrawCanvas.Children.Add(_currentLine);
-                    X1CoordinateTextBox.Text = _currentLine.X1.ToString("F0");
-                    Y1CoordinateTextBox.Text = _currentLine.Y1.ToString("F0");
-                    LineThicknessTextBox.Text = _currentLine.StrokeThickness.ToString("F0");
+                    case DrawingMode.Line:
+                        {
+                            _currentLine = new Line
+                            {
+                                Stroke = Brushes.Black,
+                                StrokeThickness = 2,
+                                X1 = _startPoint.X,
+                                Y1 = _startPoint.Y,
+                                X2 = _startPoint.X,
+                                Y2 = _startPoint.Y
+                            };
+                            _currentLine.MouseLeftButtonDown += ChangeLineProperties;
+                            DrawCanvas.Children.Add(_currentLine);
+                            X1CoordinateTextBox.Text = _currentLine.X1.ToString("F0");
+                            Y1CoordinateTextBox.Text = _currentLine.Y1.ToString("F0");
+                            LineThicknessTextBox.Text = _currentLine.StrokeThickness.ToString("F0");
+                            break;
+                        }
+                    case DrawingMode.Rectangle:
+                        {
+                            _currentRectangle = new Rectangle
+                            {
+                                Stroke = Brushes.Black,
+                                StrokeThickness = 2,
+                                Fill = Brushes.Transparent
+                            };
+
+                            Canvas.SetLeft(_currentRectangle, _startPoint.X);
+                            Canvas.SetTop(_currentRectangle, _startPoint.Y);
+
+                            DrawCanvas.Children.Add(_currentRectangle);
+
+                            X1CoordinateTextBox.Text = _startPoint.X.ToString("F0");
+                            Y1CoordinateTextBox.Text = _startPoint.Y.ToString("F0");
+                            LineThicknessTextBox.Text = _currentRectangle.StrokeThickness.ToString("F0");
+                            break;
+                        }
+                    case DrawingMode.Circle:
+                        {
+
+                            break;
+                        }
                 }
             }
         }
@@ -97,7 +123,26 @@ namespace Project1
                     }
                 case DrawingMode.Rectangle:
                     {
+                        if (_isDrawing && _currentRectangle != null)
+                        {
+                            // Obliczanie aktualnego rozmiaru prostokąta
+                            Point currentPoint = e.GetPosition(DrawCanvas);
+                            double width = currentPoint.X - _startPoint.X;
+                            double height = currentPoint.Y - _startPoint.Y;
 
+                            // Ustawiamy szerokość i wysokość prostokąta
+                            _currentRectangle.Width = Math.Abs(width);
+                            _currentRectangle.Height = Math.Abs(height);
+
+                            // Przemieszczamy prostokąt, jeśli rysujemy od prawego do lewego
+                            if (width < 0)
+                                Canvas.SetLeft(_currentRectangle, currentPoint.X);
+                            if (height < 0)
+                                Canvas.SetTop(_currentRectangle, currentPoint.Y);
+
+                            X2CoordinateTextBox.Text = currentPoint.X.ToString("F0");
+                            Y2CoordinateTextBox.Text = currentPoint.Y.ToString("F0");
+                        }
                         break;
                     }
                 case DrawingMode.Circle:
@@ -122,6 +167,7 @@ namespace Project1
         private void Reset_Button(object sender, RoutedEventArgs e)
         {
             _currentLine = null;
+            _currentRectangle = null;
             X1CoordinateTextBox.Text = "";
             Y1CoordinateTextBox.Text = "";
             X2CoordinateTextBox.Text = "";
@@ -137,10 +183,12 @@ namespace Project1
 
         }
 
-        private void ChangeProperties(object sender, MouseButtonEventArgs e)
+        private void ChangeLineProperties(object sender, MouseButtonEventArgs e)
         {
+            
             if (_currentDravingMode.Equals(DrawingMode.Cursor))
             {
+                _currentRectangle = null;
                 _offset = e.GetPosition(DrawCanvas);
                 Line clickedLine = sender as Line;
                 if (clickedLine != null)
@@ -157,46 +205,139 @@ namespace Project1
         }
         private void SetProperties_Click(object sender, RoutedEventArgs e)
         {
-            if (_currentLine == null)
+            switch (_currentDravingMode)
             {
-                if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
+                case DrawingMode.Cursor:
+                    {
+                        if (_currentLine != null)
+                        {
+                            EditLine();
+                        }
+                        else if (_currentRectangle != null)
+                        {
+
+                        }
+                        
+                        break;
+                    }
+                case DrawingMode.Line:
+                    {
+                        if (_currentLine == null)
+                        {
+                            NewLine();
+                        }
+                        else if (_currentLine != null)
+                        {
+                            EditLine();
+                        }
+                        break;
+                    }
+                case DrawingMode.Rectangle:
+                    {
+                        if(_currentRectangle == null)
+                        {
+                            NewRectangle();
+                        }
+                        break;
+                    }
+                case DrawingMode.Circle:
+                    {
+
+                        break;
+                    }
+            }
+        }
+
+        private void NewLine()
+        {
+            if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
                     double.TryParse(Y1CoordinateTextBox.Text, out double y1) &&
                     double.TryParse(X2CoordinateTextBox.Text, out double x2) &&
                     double.TryParse(Y2CoordinateTextBox.Text, out double y2) &&
                     double.TryParse(LineThicknessTextBox.Text, out double thickness))
-                {
-                    _currentLine = new Line
-                    {
-                        Stroke = Brushes.Black,
-                        StrokeThickness = thickness,
-                        X1 = x1,
-                        Y1 = y1,
-                        X2 = x2, 
-                        Y2 = y2
-                    };
-                    _currentLine.MouseLeftButtonDown += ChangeProperties;
-                    DrawCanvas.Children.Add(_currentLine);
-                }
-            }
-            else if (_currentLine != null)
             {
-
-                if (double.TryParse(LineThicknessTextBox.Text, out double thickness))
+                _currentLine = new Line
                 {
-                    _currentLine.StrokeThickness = thickness;
-                }
-
-                
-                if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
+                    Stroke = Brushes.Black,
+                    StrokeThickness = thickness,
+                    X1 = x1,
+                    Y1 = y1,
+                    X2 = x2,
+                    Y2 = y2
+                };
+                _currentLine.MouseLeftButtonDown += ChangeLineProperties;
+                DrawCanvas.Children.Add(_currentLine);
+            }
+        }
+        private void EditLine()
+        {
+            
+            if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
+                double.TryParse(Y1CoordinateTextBox.Text, out double y1) &&
+                double.TryParse(X2CoordinateTextBox.Text, out double x2) &&
+                double.TryParse(Y2CoordinateTextBox.Text, out double y2) &&
+                double.TryParse(LineThicknessTextBox.Text, out double thickness))
+            {
+                _currentLine.X1 = x1;
+                _currentLine.Y1 = y1;
+                _currentLine.X2 = x2;
+                _currentLine.Y2 = y2;
+                _currentLine.StrokeThickness = thickness;
+            }
+        }
+        private void NewRectangle()
+        {
+            if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
                     double.TryParse(Y1CoordinateTextBox.Text, out double y1) &&
                     double.TryParse(X2CoordinateTextBox.Text, out double x2) &&
-                    double.TryParse(Y2CoordinateTextBox.Text, out double y2))
+                    double.TryParse(Y2CoordinateTextBox.Text, out double y2) &&
+                    double.TryParse(LineThicknessTextBox.Text, out double thickness))
+            {
+                _currentRectangle = new Rectangle
                 {
-                    _currentLine.X1 = x1;
-                    _currentLine.Y1 = y1;
-                    _currentLine.X2 = x2;
-                    _currentLine.Y2 = y2;
-                }               
+                    Stroke = Brushes.Black,
+                    StrokeThickness = thickness,
+                    Fill = Brushes.Transparent
+                };
+
+                Canvas.SetLeft(_currentRectangle, x1);
+                Canvas.SetTop(_currentRectangle, y1);
+
+
+                double width = x2 - x1;
+                double height = y2 - y1;
+                
+                _currentRectangle.Width = Math.Abs(width);
+                _currentRectangle.Height = Math.Abs(height);
+                
+                if (width < 0)
+                    Canvas.SetLeft(_currentRectangle, x2);
+                if (height < 0)
+                    Canvas.SetTop(_currentRectangle, y2);
+
+                DrawCanvas.Children.Add(_currentRectangle);
+                
+            }
+        }
+        private void EditRectangle()
+        {
+            if (double.TryParse(X1CoordinateTextBox.Text, out double x1) &&
+                    double.TryParse(Y1CoordinateTextBox.Text, out double y1) &&
+                    double.TryParse(X2CoordinateTextBox.Text, out double x2) &&
+                    double.TryParse(Y2CoordinateTextBox.Text, out double y2) &&
+                    double.TryParse(LineThicknessTextBox.Text, out double thickness))
+            {
+                _currentLine = new Line
+                {
+                    Stroke = Brushes.Black,
+                    StrokeThickness = thickness,
+                    X1 = x1,
+                    Y1 = y1,
+                    X2 = x2,
+                    Y2 = y2
+                };
+                _currentLine.MouseLeftButtonDown += ChangeLineProperties;
+                DrawCanvas.Children.Add(_currentLine);
             }
         }
 
